108869e6b5df25d4df379b4e96be27b0fc1d26a2
cxan96 <c.p.xanten@gmail.com>
2018-06-21 10:08:08 +0200

    added figure of neuron model

--- thesis/build.sh
+++ thesis/build.sh
@@ -2,18 +2,21 @@
 
 MAIN_FILE="bachelorthesis_christian_peters"
 
+# pdflatex compile flags
+FLAGS=""
+
 # set and create output directory
 OUTPUT_DIR=latex_output
 mkdir -p $OUTPUT_DIR
 
 # compile twice when successful once
-pdflatex -output-directory="$OUTPUT_DIR" "$MAIN_FILE.tex" && pdflatex -output-directory="$OUTPUT_DIR" "$MAIN_FILE.tex"
+pdflatex $FLAGS -output-directory="$OUTPUT_DIR" "$MAIN_FILE.tex" && pdflatex $FLAGS -output-directory="$OUTPUT_DIR" "$MAIN_FILE.tex"
 
 # run biber
 biber "$OUTPUT_DIR/$MAIN_FILE"
 
 # recompile
-if pdflatex -output-directory="$OUTPUT_DIR" "$MAIN_FILE.tex"
+if pdflatex $FLAGS -output-directory="$OUTPUT_DIR" "$MAIN_FILE.tex"
 then
     # move .pdf to main directory
     mv "$OUTPUT_DIR/$MAIN_FILE.pdf" .

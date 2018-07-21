3f3ceaa404ae1600b5f6b32f18d8935735b12c56
cxan96 <c.p.xanten@gmail.com>
2018-07-21 18:22:12 +0200

    added fault plots

--- scripts/plot_histogram.py
+++ scripts/plot_histogram.py
@@ -6,12 +6,13 @@ import re
 import matplotlib
 matplotlib.use('WxAgg')
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 import seaborn as sns
 
 if __name__ == '__main__':
 
-    fileName = 'histograms/hist_01.txt'
+    fileName = 'histograms/hot_wire.txt'
 
     data = None
 
@@ -35,10 +36,19 @@ if __name__ == '__main__':
 
     # rotate data befor plotting
     data = np.rot90(data, k=1)
-    ax = sns.heatmap(data, cmap='inferno', xticklabels=10)
+    ax = sns.heatmap(data, cmap='hot', xticklabels=10)
     # set the layer labels correctly
     ax.set_yticklabels([6, 5, 4, 3, 2, 1])
     ax.set_ylabel("Layer")
     ax.set_xlabel("Wire")
+
+    # create a rectangle to illustrate the region of interest
+    # dead pin: rect = patches.Rectangle((14, 0.5), 12, 2, linewidth=2, edgecolor='blue', facecolor='none')
+    # dead connector: rect = patches.Rectangle((27, 0), 8, 6, linewidth=2, edgecolor='blue', facecolor='none')
+    # dead wire: rect = patches.Rectangle((80, 3.5), 9, 2, linewidth=2, edgecolor='blue', facecolor='none')
+    rect = patches.Rectangle((1, 1.5), 7, 2, linewidth=2, edgecolor='blue', facecolor='none')
+    ax.add_patch(rect)
+    
+    plt.tight_layout()
     plt.show()
     

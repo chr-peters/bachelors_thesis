fbc2a9a60225984c5ecc62e73655ba21a75817ae
cxan96 <c.p.xanten@gmail.com>
2018-08-06 10:39:41 +0200

    added setup for presentation

---
+++ presentation/build.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+MAIN_FILE="presentation"
+
+# pdflatex compile flags
+FLAGS=""
+
+# set and create output directory
+OUTPUT_DIR=latex_output
+mkdir -p $OUTPUT_DIR
+
+# compile twice when successful once
+pdflatex $FLAGS -output-directory="$OUTPUT_DIR" "$MAIN_FILE.tex" && pdflatex $FLAGS -output-directory="$OUTPUT_DIR" "$MAIN_FILE.tex"
+
+# run biber
+biber "$OUTPUT_DIR/$MAIN_FILE"
+
+# recompile
+if pdflatex $FLAGS -output-directory="$OUTPUT_DIR" "$MAIN_FILE.tex"
+then
+    # move .pdf to main directory
+    mv "$OUTPUT_DIR/$MAIN_FILE.pdf" .
+fi
+
